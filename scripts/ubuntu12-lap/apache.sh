98cd71d4baddcf2e96639de6185ba88ee420087d
retspen <anatoliy.guskov@gmail.com>
2014-06-16 09:17:59 +0300

    Fix httpd.conf

--- scripts/ubuntu12-lap/apache.sh
+++ scripts/ubuntu12-lap/apache.sh
@@ -2,4 +2,5 @@
 
 apt-get -y install php5 php5-gd php5-mysql php5-curl php5-cli php5-geoip php5-pgsql libapache2-mod-php5
 a2enmod rewrite
+echo 'ServerName webserv' > /etc/apache2/httpd.conf
 sed -i 's/var\/www/vagrant/g' /etc/apache2/sites-enabled/000-default
