ee767ca32264dd9ef8c58c766e373893cad0eb6d
retspen <anatoliy.guskov@gmail.com>
2014-07-08 10:45:47 +0300

    Add CentOS-7

---
+++ scripts/centos7/vboxguest.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yum -y --disablerepo=\* --enablerepo=base install wget perl gcc kernel-devel-$(uname -r)
+mkdir /tmp/virtualbox
+VERSION=$(cat /home/vagrant/.vbox_version)
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VERSION.iso /tmp/virtualbox
+sh /tmp/virtualbox/VBoxLinuxAdditions.run
+umount /tmp/virtualbox
+rmdir /tmp/virtualbox
+rm /home/vagrant/*.iso
