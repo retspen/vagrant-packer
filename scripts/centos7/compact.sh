7f85c0e274317c98268a281126c88cc9c2560b21
retspen <anatoliy.guskov@gmail.com>
2014-07-22 14:43:49 +0300

    Remove not need files

--- scripts/centos7/compact.sh
+++ scripts/centos7/compact.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yum -y remove binutils glibc-headers glibc-devel libmpc mpfr kernel-devel kernel-headers cpp gcc perl* 
+yum -y remove glibc-headers glibc-devel libmpc mpfr kernel-devel kernel-headers cpp gcc bzip2 wget perl* 
 yum clean all
 
 dd if=/dev/zero of=/junk bs=1M
--- scripts/centos7/vboxguest.sh
+++ scripts/centos7/vboxguest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yum -y --disablerepo=\* --enablerepo=base install wget perl gcc kernel-devel-$(uname -r)
+yum -y --disablerepo=\* --enablerepo=base install bzip2 wget perl gcc kernel-devel-$(uname -r)
 mkdir /tmp/virtualbox
 VERSION=$(cat /home/vagrant/.vbox_version)
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VERSION.iso /tmp/virtualbox
